6a414c5439d5f43ce00f07af4e2935b8f986c512
Wang Guan <momocraft@gmail.com>
2024-02-11 01:11:12 +0900

    add fps meter

--- demo/src/pages/v1/multidevice-gesture/:namespace.tsx
+++ demo/src/pages/v1/multidevice-gesture/:namespace.tsx
@@ -1,9 +1,11 @@
 import {PageProps} from '../../_shared';
 import {HammerTouchDemo} from '../../../apps/v1/hammer/demo';
 import {useRandomId} from '../../../hooks/use-random-id';
+import {useFps} from '@jokester/ts-commonutil/lib/react/hook/use-fps';
 
 export function MultiDeviceGesturePage(props: PageProps<{namespace: string}>) {
   const ownId = useRandomId(undefined, 8);
+  const fps = useFps(120);
   return (
     <div className="container">
       namespace: {props.matches!.namespace}
@@ -12,6 +14,8 @@ export function MultiDeviceGesturePage(props: PageProps<{namespace: string}>) {
         namespace={props.matches!.namespace}
         ownClientId={ownId}
       />
+      <hr />
+      <p>fps: {fps.toFixed(2)}</p>
     </div>
   );
 }
