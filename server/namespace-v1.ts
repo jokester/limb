11343395fbeffca9c4a4002f6af6e945e07470c0
Wang Guan <momocraft@gmail.com>
2024-05-03 23:46:22 +0900

    traditional server: export socket.io handler

--- server/namespace-v1.ts
+++ server/namespace-v1.ts
@@ -1,25 +1,28 @@
 import debug from 'debug';
 import type sio from 'socket.io';
 
+export const parentNamespace = /^\/v1\/[-\w:.]+$/;
+
 const logger = debug('limb:server:v1');
 
-export function onV1Connection(namespace: sio.Namespace, socket: sio.Socket) {
+export function onV1Connection(socket: sio.Socket) {
+  const namespace = socket.nsp;
   logger('connection', namespace.name, socket.id);
 
-  socket.on('disconnecting', reason => {
+  socket.on('disconnecting', (reason: any) => {
     logger('disconnecting', namespace.name, socket.id, reason);
   });
 
-  socket.on('disconnect', reason => {
+  socket.on('disconnect', (reason: any) => {
     logger('disconnect', namespace.name, socket.id, reason);
   });
 
-  socket.on('error', error => {
+  socket.on('error', (error: any) => {
     logger('error', namespace.name, socket.id, error);
   });
 
   // only forward "message" events. Clients should use `send(clientEventName, value)`
-  socket.on('message', (event, value) => {
+  socket.on('message', (event: string, value: any) => {
     logger('forwarding message', namespace.name, socket.id, event, value);
     namespace.send(event, value);
   });
